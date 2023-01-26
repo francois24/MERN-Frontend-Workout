2233a5abf15da4577e92c89d9ed561c4c51b3582
Tom <ffrancois737@gmail.com>
2023-01-26 11:42:28 +0800

    Updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
+
 
 //components
 import WorkoutDetail from '../components/WorkoutDetail'
@@ -7,18 +9,27 @@ import WorkoutForm from "../components/WorkoutsForm"
 
 const Home = () => {
     const {workouts,dispatch} = useWorkoutsContext()
+    const {user} = useAuthContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
+            const response = await fetch('/api/workouts',{
+                headers: {
+                    'Authorization':`Bearer ${user.token}`
+                }
+            })
             const json = await response.json()
 
             if (response.ok){
                 dispatch({type: 'SET_WORKOUTS', payload: json})
             }
-        }
+        }   
+
+        if (user){
             fetchWorkouts()
-    },[dispatch])
+        }
+            
+    },[dispatch,user])
 
     return (
         <div className="home">
